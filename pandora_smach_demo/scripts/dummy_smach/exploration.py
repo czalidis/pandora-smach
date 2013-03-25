01ca1cca4f57219d13cc86b7c16bcae44bc617e9
Chris Zalidis <zalidis@gmail.com>
2013-03-25 18:34:09 +0200

    added iterator_tutorial to use it with target selection/sending container

--- pandora_smach_demo/scripts/dummy_smach/exploration.py
+++ pandora_smach_demo/scripts/dummy_smach/exploration.py
@@ -36,14 +36,18 @@ class DecideVictimState(State):
 		else:
 			rospy.logerr('Wrong message type!')
 
-
+def _termination_cb(outcome_map):
+	print outcome_map
+	if outcome_map['TARGET_CONTROLLER'] == 'target_sent' and outcome_map['VICTIM_MONITOR'] == None:
+		return False
+	return True
 
 def ExplorationContainer():
 	cc = Concurrence(outcomes=['next_target','victim_thermal','victim_camera','aborted','preempted'], 
 	default_outcome='next_target', outcome_map={'next_target':{'TARGET_CONTROLLER':'target_sent'},
 	'victim_thermal':{'VICTIM_MONITOR':'victim_thermal'}, 'victim_camera':{'VICTIM_MONITOR':'victim_camera'}, 
 	'preempted':{'TARGET_CONTROLLER':'preempted','VICTIM_MONITOR':'preempted'}, 'aborted':{'TARGET_CONTROLLER':'aborted'}},
-	child_termination_cb=lambda so: True)
+	child_termination_cb=_termination_cb)
 	
 	with cc:
 		Concurrence.add('TARGET_CONTROLLER', utils.TargetSelectorContainer('explore'))
