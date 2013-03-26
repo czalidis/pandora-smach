97ab4f6aa4e55218be346db6751d1a5ebe0690ee
Chris Zalidis <zalidis@gmail.com>
2013-03-26 20:16:13 +0200

    WIN!! fsm working, ready for robocup 2013! Eindhoven we are coming!

--- pandora_smach_demo/scripts/dummy_smach/exploration.py
+++ pandora_smach_demo/scripts/dummy_smach/exploration.py
@@ -7,25 +7,26 @@ import smach_ros
 from pandora_smach_demo_msgs.msg import VictimFound
 
 from smach import State, StateMachine, Concurrence
-from smach_ros import SimpleActionState, MonitorState
+from smach_ros import SimpleActionState
+from my_monitor_state import MyMonitorState
 
 import utils
 
-class MonitorVictimState(MonitorState):
-	def __init__(self):
-		MonitorState.__init__(self,'/victim_found', VictimFound, self.monitor_cb)
+class MonitorVictimState(MyMonitorState):
+	def __init__(self, input_keys=[], output_keys=[]):
+		MyMonitorState.__init__(self,'/victim_found', VictimFound, 
+		self.monitor_cb, ik=input_keys, ok=output_keys)
 		
 	def monitor_cb(self, userdata, msg):
-		return False
-		#~ if msg.victimNotificationType == msg.TYPE_THERMAL or msg.victimNotificationType == msg.TYPE_CAMERA:
-			#~ userdata.victim_type = msg.victimNotificationType
-			#~ return True
-		#~ else:
-			#~ return False
+		if msg.victimNotificationType == msg.TYPE_THERMAL or msg.victimNotificationType == msg.TYPE_CAMERA:
+			userdata[0].victim_type = msg.victimNotificationType
+			return False
+		else:
+			return True
 
 class DecideVictimState(State):
 	def __init__(self):
-		State.__init__(self,outcomes=['thermal','camera'])
+		State.__init__(self,outcomes=['thermal','camera'], input_keys=['victim_type'])
 	
 	def execute(self, userdata):
 		victim_msg = VictimFound()
@@ -34,12 +35,12 @@ class DecideVictimState(State):
 		elif userdata.victim_type == victim_msg.TYPE_CAMERA:
 			return 'camera'
 		else:
-			rospy.logerr('Wrong message type!')
+			rospy.logerr('Wrong message type!'+' victim_type = '+str(userdata.victim_type))
 
 def _termination_cb(outcome_map):
-	print outcome_map
-	if outcome_map['TARGET_CONTROLLER'] == 'target_sent' and outcome_map['VICTIM_MONITOR'] == None:
-		return False
+	#~ print outcome_map
+	#~ if outcome_map['TARGET_CONTROLLER'] == 'target_sent' and outcome_map['VICTIM_MONITOR'] == None:
+		#~ return False
 	return True
 
 def ExplorationContainer():
@@ -56,8 +57,9 @@ def ExplorationContainer():
 		sm_victim_monitor.userdata.victim_type = 0
 		with sm_victim_monitor:
 			
-			StateMachine.add('VICTIM_MONITORING', MonitorVictimState(), 
-			transitions={'invalid':'VICTIM_MONITORING', 'valid':'VICTIM_DECIDE', 'preempted':'preempted'}, 
+			StateMachine.add('VICTIM_MONITORING', MonitorVictimState(
+			input_keys=['victim_type'], output_keys=['victim_type']), 
+			transitions={'invalid':'VICTIM_DECIDE', 'valid':'VICTIM_MONITORING', 'preempted':'preempted'}, 
 			remapping={'victim_type':'victim_type'})
 			
 			StateMachine.add('VICTIM_DECIDE', DecideVictimState(), 
